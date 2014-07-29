3cfb67a4ef301d9227ade7d5eb7ee617c37f0678
Peace Ngara <peacengara@aol.com>
2014-07-29 12:29:37 +0200

    Mail Convertor File
    This Page is the main page where the convertor does its magic ..

---
+++ econvertor.php
@@ -0,0 +1,16 @@
+<?php
+
+/*============================== Separate Semi-Colon Delimited Emails into arrays or Individual Pieces =====================
+This Programme was Written by Peace Ngara after a Web Project that had email lists separated by a semicolon ;
+e.g peacengara@aol.com ; peacengara@yahoo.com ;
+The email lists were more than 5000 making it impossible to copy and paste in an Email Sending Programme.
+
+/* ============================== The Challenge =================================== 
+
+Export to Excel or Comma Delimited Listst */
+
+
+
+
+
+?>
